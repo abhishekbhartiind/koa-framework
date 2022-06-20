e86d1d6f5a45ccaefae413cecae1ac54dcb19639
Abhishek Bharti <abhishek.bharti@myzeller.com>
2022-06-20 21:09:58 +0530

    added more routes

--- app.js
+++ app.js
@@ -2,16 +2,45 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
 
-const app = new Koa(); 
-const router = new Router();
+const app = new Koa();
+
+// log all events to the terminal
 app.use(logger());
 
-router.get('/', (ctx, next) => {
-  ctx.body = 'Hello World!';
+// error handling
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    ctx.app.emit('error', err, ctx);
+  }
 });
 
+// instantiate our new Router
+const router = new Router();
+const dogRouter = new Router({
+  prefix: '/dogs'
+});
+// require our external routes and pass in the router
+require('./routes/basic')({ router });
+require('./routes/dogs')({ dogRouter });
+
+// tells the router to use all the routes that are on the object
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+app.use(dogRouter.routes());
+app.use(dogRouter.allowedMethods());
+
+// router.get('/', (ctx, next) => {
+//   ctx.body = 'Hello World!';
+// });
+
+// app.use(router.routes());
+// app.use(router.allowedMethods());
+
+// tell the server to listen to events on a specific port
 const server = app.listen(3000);
 module.exports = server;
